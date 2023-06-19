56235642b957a0cdfbbc59caa23b734f7b520a42
mothy <moshimoshi6k@gmail.com>
2023-06-18 21:44:07 -0500

    added flatpicker

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,8 +1,10 @@
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Laravel 10 CRUD Application - ItSolutionStuff.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    @stack('style')
 </head>
 <body>
 
@@ -10,6 +12,6 @@
     @include('nav')
     @yield('content')
 </div>
-
+@stack('scripts')
 </body>
 </html>
